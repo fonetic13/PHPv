cb1ecb9a3e1c1616530a2d519b787379bf85453b
fonetic13 <ivbistricic@gmail.com>
2016-03-29 12:45:42 +0200

    refresh

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+/******************************POLJA*SA*FOR*PETLJOM******************************************************
+$auti = array('BMW', 'AUDI', 'PORCHE', 'RENAULT'); // Deklariranje array-a i unos vrijednosti u array.
+for ($i = 0; $i <= 3; $i++){                       // Ispis svakog elementa polja zasebno kroz for petlju.
+	if($i<3){
+		echo $auti[$i] . ', ';
+	}
+	else
+	{
+		echo $auti[$i] . '.';
+	}
+}
+echo '<br>'; //Nova linija
+echo print_r($auti); //Ispisuje cijeli sadržaj polja sa vrijednostima mjesta.
+**********************************************************************************************************/
+/***************USPOREDJIVANJE*2*STRINGA****
+$text1 = 'Ovaj tekst uopce nije plagijat onome koji sljedi';
+$text2 = 'O ovom tekstu smo pricali u prethodnoj varijabli';
+similar_text($text1, $text2, $rezultat); //Funkcija usporedjuje sličnost 2 stringa te vraća vrijednost u postotku.
+	echo 'Rezultat sličnosti je: ' . $rezultat . '%';
+*******************************************/
+/***********OBRNUTI*ISPIS*******************
+$string = 'Ovdje ima cetiri rijeci';
+$string_reversed = strrev($string);  //funkcija koja ispisuje string obrnuto.
+echo $string_reversed; 
+*********************************************/
+/*****************RANDOM*MJESA*SLOVA**********
+$string = 'Ovdje ima cetiri rijeci';
+$string_shuffled = str_shuffle($string); //spremamo u varijablu funkciju koja randomizira slova iu $string-a.
+$half = substr($string_shuffled, 0, strlen($string)/2); //spremamo u varijablu $half funkciju koja uzima $string_shuffled te ogranicava broj znakova uzetih u obzir u ovom slučaju pola od $string
+echo $string_shuffled . '<br>';
+echo strlen($string);
+echo '<br>' . $half;
+**********************************************/
+/*************BROJI*RIJECI********************
+$string = 'Ovdje ima cetiri rijeci.';
+$string_word_count = str_word_count($string, 2, '.');  // Funkcija koja broji riječi ili ih smjesta u array ovisno dal je 0,1 ili 2. echo samo za 0.
+print_r($string_word_count);
+**********************************************/
+/**********PRIKAZ*IP*ADRESE*KROZ*FUNKCIJU******
+$tvoj_ip = $_SERVER['REMOTE_ADDR'];
+function prikaziIp(){
+	global $tvoj_ip;
+	echo 'Tvoja IP adresa je: ' . $tvoj_ip;
+}
+prikaziIp();
+***********************************************/
+/**********NAPREDNIJA**FUNKCIJA*****************
+function displayDate($day,$date,$year){
+	echo $day . '/' . $date . '/' . $year;
+	
+}
+displayDate('Ponedjeljak',28,2019);
+***********************************************/
+/**********NAPREDNIJA**FUNKCIJA*****************
+$unbr1 = 10;
+$unbr2 = 12;
+function zbroj($br1,$br2){
+	echo $br1 + $br2;
+	
+}
+zbroj($unbr1,$unbr2);
+***********************************************/
+/*********JEDNOSTAVNA**FUNKCIJA*****************
+function ispis(){
+	echo 'Ivan.';
+}
+echo 'Ime mi je ';
+ispis();
+************************************************/
+/************************SPAJANJE NA BAZU************************************************
+@mysql_connect('localhost','root','') or die('GREŠKA. NIJE MOGUĆE SPOJITI SE NA BAZU!'); // spajanje na bazu imena localhost sa korisnikom root bez sifre. Ako spajanje nije uspjelo javi navedenu gresku.
+echo 'Spojen si.';
+*****************************************************************************************/
+/******************SWITCH************************ 
+$dan = 'Subota';
+switch ($dan){
+	case 'Subota':
+	case 'Nedjelja':
+		echo 'Wooohoooo vikend je!!!';
+	break;
+	
+	default:
+		echo 'moras raditi.';
+	break;
+}***********************************************/
+?>
